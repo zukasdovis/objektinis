de70c08213d88cc243034b9977a3220a56d6fd8b
zukasdovis <zukasdovis@gmail.com>
2026-03-05 19:28:48 +0200

    Update rusiavimas.h

--- rusiavimas.h
+++ rusiavimas.h
@@ -1,1 +1,11 @@
+#ifndef RUSIAVIMAS_H
+#define RUSIAVIMAS_H
 
+#include "studentas.h"
+
+bool pagalVarda(const studentai &, const studentai &);
+bool pagalPavarde(const studentai &, const studentai &);
+bool pagalGalutiniVid(const studentai &, const studentai &);
+bool pagalGalutiniMed(const studentai &, const studentai &);
+
+#endif
