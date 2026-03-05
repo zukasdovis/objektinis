fc064e84436f7557a134ba4be4101e1adf60c970
zukasdovis <zukasdovis@gmail.com>
2026-03-05 19:30:48 +0200

    Update output.cpp

--- output.cpp
+++ output.cpp
@@ -1,1 +1,34 @@
+#include "output.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 
+using namespace std;
+
+void outputas(vector<studentai> &grupe)
+{
+    cout << fixed << setprecision(2);
+
+    for (const auto &A : grupe)
+    {
+        cout << left << setw(10) << A.vardas
+             << setw(10) << A.pavarde
+             << setw(10) << A.galutinis_vid
+             << setw(10) << A.galutinis_med << endl;
+    }
+}
+
+void outputas_fr(vector<studentai> &grupe)
+{
+    ofstream fr("rezultatai.txt");
+
+    fr << fixed << setprecision(2);
+
+    for (const auto &A : grupe)
+    {
+        fr << left << setw(10) << A.vardas
+           << setw(10) << A.pavarde
+           << setw(10) << A.galutinis_vid
+           << setw(10) << A.galutinis_med << endl;
+    }
+}
