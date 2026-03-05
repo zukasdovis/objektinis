8f495d0d4a56eb4db94b177aac642542ea4852ac
zukasdovis <zukasdovis@gmail.com>
2026-03-05 19:29:56 +0200

    Add header file for output functions

--- output.h
+++ output.h
@@ -1,1 +1,10 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
 
+#include "studentas.h"
+#include <vector>
+
+void outputas(std::vector<studentai> &grupe);
+void outputas_fr(std::vector<studentai> &grupe);
+
+#endif
