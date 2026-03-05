58958ab49f31cd5a31efccc2a349c4086a545057
zukasdovis <zukasdovis@gmail.com>
2026-03-05 19:29:26 +0200

    Add comparison functions for studentai structure

--- rusiavimas.cpp
+++ rusiavimas.cpp
@@ -1,1 +1,21 @@
+#include "rusiavimas.h"
 
+bool pagalVarda(const studentai &A, const studentai &B)
+{
+    return A.vardas < B.vardas;
+}
+
+bool pagalPavarde(const studentai &A, const studentai &B)
+{
+    return A.pavarde < B.pavarde;
+}
+
+bool pagalGalutiniVid(const studentai &A, const studentai &B)
+{
+    return A.galutinis_vid < B.galutinis_vid;
+}
+
+bool pagalGalutiniMed(const studentai &A, const studentai &B)
+{
+    return A.galutinis_med < B.galutinis_med;
+}
