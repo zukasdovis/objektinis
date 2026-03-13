1938cfe68a3c5b640ac6307b755c723c9f48c863
zukasdovis <zukasdovis@gmail.com>
2026-03-13 21:15:57 +0200

    Add file handling and sorting function declarations

--- rusiavimas.h
+++ rusiavimas.h
@@ -2,10 +2,12 @@
 #define RUSIAVIMAS_H
 
 #include "studentas.h"
-
+#include "failai.h"
+#include <bits/stdc++.h>
 bool pagalVarda(const studentai &, const studentai &);
 bool pagalPavarde(const studentai &, const studentai &);
 bool pagalGalutiniVid(const studentai &, const studentai &);
 bool pagalGalutiniMed(const studentai &, const studentai &);
+void failoRusiavimas(int kiekis);
 
 #endif
