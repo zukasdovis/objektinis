399c2cdc278e82a0f4ab2dfbcbe69e4b408eed3b
zukasdovis <zukasdovis@gmail.com>
2026-03-13 21:14:47 +0200

    Implement timing for file generation
    Added timing functionality to track file generation duration.

--- generatorius.cpp
+++ generatorius.cpp
@@ -6,40 +6,39 @@
 
 void generuotiFaila(int kiekis)
 {
+    clock_t start = clock();
     std::string failas = "studentai_" + std::to_string(kiekis) + ".txt";
     std::ofstream out(failas);
 
-    out << "Vardas Pavarde ND1 ND2 ND3 ND4 ND5 ND6 ND7 ND8 ND9 ND10 Galutinis\n";
+    out << "Vardas Pavarde ND1 ND2 ND3 ND4 ND5 ND6 ND7 ND8 ND9 ND10 Egz\n";
 
     for (int i = 1; i <= kiekis; i++)
     {
 
-        out << "Vardas" << i << " "
-            << "Pavarde" << i << " ";
+        out << "Vardas" << i << " " << "Pavarde" << i << " ";
 
         for (int i = 0; i < 10; i++)
         {
             int paz = rand() % 10 + 1;
             out << paz << " ";
         }
-        int galutinis = rand() % 10 + 1;
-        out << galutinis << "\n";
+        int egzaminas = rand() % 10 + 1;
+        out << egzaminas << "\n";
     }
 
     out.close();
+    clock_t end = clock();
+    double laikas = double(end - start) / CLOCKS_PER_SEC;
+
+    std::cout << "Failas " << kiekis << " dydzio sukurtas per " << laikas << " s\n";
 }
 
 void generuotiVisus()
 {
-    clock_t start = clock();
+
     generuotiFaila(1000);
     generuotiFaila(10000);
     generuotiFaila(100000);
     generuotiFaila(1000000);
     generuotiFaila(10000000);
-
-    clock_t end = clock();
-    double laikas = double(end - start) / CLOCKS_PER_SEC;
-
-    std::cout << "Failai sukurtas per " << laikas << " s\n";
 }
