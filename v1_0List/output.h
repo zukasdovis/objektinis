fd53fe1e77d398b51e4430fc8907aa323c905d77
zukasdovis <zukasdovis@gmail.com>
2026-03-26 23:31:01 +0200

    Add files via upload

---
+++ v1_0List/output.h
@@ -0,0 +1,10 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include "studentas.h"
+#include <vector>
+#include <list>
+void outputas(std::list<studentai> grupe);
+void outputas_fr(std::list<studentai> grupe);
+
+#endif
