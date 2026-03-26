45ed5df0fa04acb7406014210016e4275908ccb1
zukasdovis <zukasdovis@gmail.com>
2026-03-26 22:43:57 +0200

    Add files via upload

---
+++ v1_0Vector/generatorius.h
@@ -0,0 +1,7 @@
+#ifndef GENERATORIUS_H
+#define GENERATORIUS_H
+
+void generuotiVisus();
+void generuotiFaila(int kiekis);
+
+#endif
---
+++ v1_0Vector/rusiavimas.h
@@ -0,0 +1,15 @@
+#ifndef RUSIAVIMAS_H
+#define RUSIAVIMAS_H
+
+#include "studentas.h"
+#include "failai.h"
+#include <bits/stdc++.h>
+bool pagalVarda(const studentai &, const studentai &);
+bool pagalPavarde(const studentai &, const studentai &);
+bool pagalGalutiniVid(const studentai &, const studentai &);
+bool pagalGalutiniMed(const studentai &, const studentai &);
+template <typename Container>
+void splitContainer(Container &grupe, Container &silpniakai);
+void failoRusiavimas(int kiekis);
+
+#endif
