f7d0a6fc49bd4bc2e8da69d27e4013647264ff0c
zukasdovis <zukasdovis@gmail.com>
2026-03-26 20:37:48 +0200

    Add files via upload

---
+++ v1_0Deque/failai.h
@@ -0,0 +1,10 @@
+#ifndef FAILAI_H
+#define FAILAI_H
+
+#include "studentas.h"
+#include <deque>
+#include <string>
+
+void failu_nusk(std::deque<studentai> &grupe, std::string failas);
+
+#endif
---
+++ v1_0Vector/tyrimai.h
@@ -0,0 +1,11 @@
+#ifndef TYRIMAI_H
+#define TYRIMAI_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void tyrimai();
+void pirmasTyrimas();
+void antrasTyrimas();
+
+#endif
